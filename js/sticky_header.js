6f75c8dae9111006106f0146283b76dd224bcad6
folmert <folmert2@gmail.com>
2015-07-26 14:35:48 +0200

    minor fixes after jshint

--- js/sticky_header.js
+++ js/sticky_header.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var header = document.querySelectorAll('header')[0];
 var headerHeight = header.offsetHeight;
@@ -11,29 +11,27 @@ window.addEventListener('scroll', function() {
 
 	if (window.pageYOffset > headerHeight/2) {
 		body.classList.add('js-sticky_header');
-		body.style.paddingTop = headerHeight + "px";
-		header.style.height = headerHeight/2 + "px";
+		body.style.paddingTop = headerHeight + 'px';
+		header.style.height = headerHeight/2 + 'px';
 
-		var i;
 		for (var i = headerLink.length - 1; i >= 0; i--) {
-			headerLink[i].style.lineHeight = headerHeight/2 + "px";
-		};
+			headerLink[i].style.lineHeight = headerHeight/2 + 'px';
+		}
 		
 	}
 	else {
 		body.classList.remove('js-sticky_header');
-		body.style.paddingTop = 0 + "px";
-		header.style.height = headerHeight + "px";
+		body.style.paddingTop = 0 + 'px';
+		header.style.height = headerHeight + 'px';
 
-		var i;
-		for (var i = headerLink.length - 1; i >= 0; i--) {
-			headerLink[i].style.lineHeight = headerLinkHeight + "px";
-		};
+		for (var j = headerLink.length - 1; j >= 0; j--) {
+			headerLink[j].style.lineHeight = headerLinkHeight + 'px';
+		}
 
 	}
 	
 });
 
 setTimeout(function(){
-	window.scrollTo(0,0)
-	,10});
+	window.scrollTo(0,0);
+},10);
