eff6e17440d3349f0341869c12428696c01e6472
folmert <folmert2@gmail.com>
2015-07-26 13:46:15 +0200

    initial commit

---
+++ js/sticky_header.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var header = document.querySelectorAll('header')[0];
+var headerHeight = header.offsetHeight;
+
+var headerLink = document.querySelectorAll('header ul li a');
+var headerLinkHeight = headerLink[0].offsetHeight;
+
+var body = document.querySelectorAll('body')[0];
+window.addEventListener('scroll', function() {
+
+	if (window.pageYOffset > headerHeight/2) {
+		body.classList.add('js-sticky_header');
+		body.style.paddingTop = headerHeight + "px";
+		header.style.height = headerHeight/2 + "px";
+
+		var i;
+		for (var i = headerLink.length - 1; i >= 0; i--) {
+			headerLink[i].style.lineHeight = headerHeight/2 + "px";
+		};
+		
+	}
+	else {
+		body.classList.remove('js-sticky_header');
+		body.style.paddingTop = 0 + "px";
+		header.style.height = headerHeight + "px";
+
+		var i;
+		for (var i = headerLink.length - 1; i >= 0; i--) {
+			headerLink[i].style.lineHeight = headerLinkHeight + "px";
+		};
+
+	}
+	
+});
+
+setTimeout(function(){
+	window.scrollTo(0,0)
+	,10});
