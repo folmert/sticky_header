b742f344b98d27636d85dd54b4354b534682b6a0
folmert <folmert2@gmail.com>
2015-08-03 14:34:59 +0200

    wrapped in module pattern

--- js/sticky_header.js
+++ js/sticky_header.js
@@ -1,40 +1,44 @@
 'use strict';
-querySelectorAll = document.querySelectorAll.bind(document);
+
+var sticky_header = (function () { 
+
+	var querySelectorAll = document.querySelectorAll.bind(document);
 	
+	var header = querySelectorAll('header')[0];
+	var headerHeight = header.offsetHeight;
 
-var header = querySelectorAll('header')[0];
-var headerHeight = header.offsetHeight;
+	var headerLink = querySelectorAll('header ul li a');
+	var headerLinkHeight = headerLink[0].offsetHeight;
 
-var headerLink = querySelectorAll('header ul li a');
-var headerLinkHeight = headerLink[0].offsetHeight;
+	var body = querySelectorAll('body')[0];
+	window.addEventListener('scroll', function () {
 
-var body = querySelectorAll('body')[0];
-window.addEventListener('scroll', function () {
+		if (window.pageYOffset > headerHeight/2) {
+			body.classList.add('js-sticky_header');
+			body.style.paddingTop = headerHeight + 'px';
+			header.style.height = headerHeight/2 + 'px';
 
-	if (window.pageYOffset > headerHeight/2) {
-		body.classList.add('js-sticky_header');
-		body.style.paddingTop = headerHeight + 'px';
-		header.style.height = headerHeight/2 + 'px';
+			for (var i = headerLink.length - 1; i >= 0; i--) {
+				headerLink[i].style.lineHeight = headerHeight/2 + 'px';
+			}
 
-		for (var i = headerLink.length - 1; i >= 0; i--) {
-			headerLink[i].style.lineHeight = headerHeight/2 + 'px';
 		}
-		
-	}
-	else {
-		body.classList.remove('js-sticky_header');
-		body.style.paddingTop = 0 + 'px';
-		header.style.height = headerHeight + 'px';
+		else {
+			body.classList.remove('js-sticky_header');
+			body.style.paddingTop = 0 + 'px';
+			header.style.height = headerHeight + 'px';
 
-		for (var j = headerLink.length - 1; j >= 0; j--) {
-			headerLink[j].style.lineHeight = headerLinkHeight + 'px';
+			for (var j = headerLink.length - 1; j >= 0; j--) {
+				headerLink[j].style.lineHeight = headerLinkHeight + 'px';
+			}
+
 		}
 
-	}
-	
-});
+	});
 
-setTimeout(function(){
+	setTimeout(function(){
 	// window.scrollTo(0,0);
 	window.scrollTo(0,window.pageYOffset);	// trigger scroll event on page reload
 },10);
+
+})();
